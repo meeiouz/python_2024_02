02f68fcc15b22b1f655b2ba6f717d49d444c41a1
meeiouz <158238084+meeiouz@users.noreply.github.com>
2024-01-31 10:48:15 +0900

    Add files via upload

---
+++ day01/var.py
@@ -0,0 +1,17 @@
+#var
+#variable - 변수
+
+# myname = input("너의 이름은?")
+# age = 20 + 1
+# print(myname)
+# print(age)
+
+name = input("당신의 이름은?")
+age = input("당신의 나이?")
+food = input("당신의 최애 음식은?")
+person  = input("당신의 최애 음식 유튜버는?")
+
+print(name)
+print(age)
+print(food)
+print(person)
